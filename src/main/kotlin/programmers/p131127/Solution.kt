0fec399266800e9c49332f906c8a06f625f76021
dongho <dongho108@naver.com>
2023-04-11 00:03:23 +0900

    임시커밋

---
+++ src/main/kotlin/programmers/p131127/Solution.kt
@@ -0,0 +1,10 @@
+package programmers.p131127
+
+class Solution {
+    fun solution(want: Array<String>, number: IntArray, discount: Array<String>): Int {
+        var answer: Int = 0
+        val wish = mutableMapOf<String, Int>()
+
+        return answer
+    }
+}
